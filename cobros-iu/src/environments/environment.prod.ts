d09f87b8c25e525fdb1b6bb969e37c07a44bbd1e
Boris G <borisgr04@hotmail.com>
2026-05-20 12:41:17 -0500

    fix: update production apiUrl to Render backend

--- cobros-iu/src/environments/environment.prod.ts
+++ cobros-iu/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   useMocks: false,
   allowDevLogin: true,   // temporal hasta configurar Google OAuth
-  apiUrl: 'https://ca-cobrosapi-prod.lemonriver-5c3c01a1.eastus.azurecontainerapps.io'
+  apiUrl: 'https://cobros-g3wi.onrender.com'
 };
