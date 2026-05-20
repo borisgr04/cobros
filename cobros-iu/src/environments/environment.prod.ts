c064129f849f945173eec3d170dd8e88cf154874
Boris G <borisgr04@hotmail.com>
2026-05-20 17:55:21 -0500

    fix: disable development login in production environment configuration

--- cobros-iu/src/environments/environment.prod.ts
+++ cobros-iu/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   useMocks: false,
-  allowDevLogin: true,   // temporal hasta configurar Google OAuth
+  allowDevLogin: false,   // temporal hasta configurar Google OAuth
   apiUrl: 'https://cobros-g3wi.onrender.com'
 };
